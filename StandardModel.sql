cb05bcd19b0148c81348e0cadbb153fbd61ebc2b
CHWS-2002 <82887651+CHDS-2002@users.noreply.github.com>
2025-01-06 12:46:03 +0300

    Update StandardModel.sql
    Upgrade of StandardModel.sql

--- StandardModel.sql
+++ StandardModel.sql
@@ -1,5 +1,5 @@
 -- Таблица для хранения информации о частицах
-create table particles (
+CREATE TABLE particles (
 	particle_id INTEGER PRIMARY KEY AUTOINCREMENT,
   	name TEXT NOT NULL,
   	symbol TEXT NOT NULL,
@@ -10,14 +10,14 @@ create table particles (
 );
 
 -- Таблица для хранения информации о взаимодействиях
-create table interactions (
+CREATE TABLE interactions (
 	interaction_id INTEGER PRIMARY KEY AUTOINCREMENT,
   	name TEXT NOT NULL,
   	description TEXT
 );
 
 -- Таблица для связи частиц и взаимодействий
-create table particle_interactions (
+CREATE TABLE particle_interactions (
   	particle_id INTEGER,
   	interaction_id INTEGER,
   	FOREIGN KEY (particle_id) REFERENCES particles(particle_id),
@@ -32,11 +32,11 @@ CREATE TABLE gravitons (
   	mass REAL NOT NULL,
   	spin REAL NOT NULL,
   	charge REAL NOT NULL,
-  	type TEXT check (type = 'Graviton')
+  	type TEXT CHECK (type = 'Graviton')
 );
 
 -- Таблица для связи гравитонов и взаимодействий
-CREATE table graviton_interactions (
+CREATE TABLE graviton_interactions (
 	graviton_id INTEGER,
   	interaction_id INTEGER,
   	FOREIGN KEY (graviton_id) REFERENCES gravitons(graviton_id),
@@ -48,14 +48,14 @@ INSERT INTO particles (name, symbol, mass, spin, charge, type) VALUES
 ('Electron', 'e-', 0.000511, 0.5, -1, 'Fermion'),
 ('Photon', 'y', 0, 1, 0, 'Boson');
 
-INSERT into interactions (name, description) VALUES
+INSERT INTO interactions (name, description) VALUES
 ('Electromagnetic Interaction', 'Interaction mediated by photons'),
 ('Strong Interaction', 'Interaction mediated by gluons'),
 ('Weak Interaction', 'Interaction mediated by W and Z bosons'),
 ('Gravitational Interactions', 'Hypothetical interaction mediated by gravitons');
 
-insert into particle_interactions (particle_id, interaction_id) VALUES
+INSERT INTO particle_interactions (particle_id, interaction_id) VALUES
 ('Graviton', 'G', 0, 2, 0, 'Graviton');
 
-insert into graviton_interactions (graviton_id, interaction_id) VALUES
+INSERT INTO graviton_interactions (graviton_id, interaction_id) VALUES
 (1, 4); -- Graviton mediates gravitational interaction
