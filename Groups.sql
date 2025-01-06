df465edbd9e157cdf61468d36a8342eb2acce728
CHWS-2002 <82887651+CHDS-2002@users.noreply.github.com>
2025-01-06 12:44:08 +0300

    Update Groups.sql
    Upgrade of the Groups.sql

--- Groups.sql
+++ Groups.sql
@@ -6,25 +6,25 @@ CREATE TABLE groups (
 );
 
 -- Создаем таблицу для хранения элементов группы
-create table elements (
-	element_id INTEGER primary key AUTOINCREMENT,
+CREATE TABLE elements (
+	element_id INTEGER PRIMARY KEY AUTOINCREMENT,
   	group_id INTEGER,
-  	value TEXT NOT NULL,
+  	VALUE TEXT NOT NULL,
   	FOREIGN KEY (group_id) REFERENCES groups(group_id)
 );
 
 -- Создаем таблицу для хранения операций между элементами
-create table operations (
+CREATE TABLE operations (
 	operation_id INTEGER PRIMARY KEY AUTOINCREMENT,
   	first_element_id INTEGER,
   	second_element_id INTEGER,
   	result_element_id INTEGER,
   	FOREIGN KEY (first_element_id) REFERENCES elements(element_id),
   	FOREIGN KEY (second_element_id) REFERENCES elements(element_id),
-  	foreign key (result_element_id) references elements(element_id)
+  	FOREIGN KEY (result_element_id) REFERENCES elements(element_id)
 );
 
 -- Пример добавления данных
-insert into groups (name, order_of_group) VALUES ('Z2', 2);
-insert into elements (group_id, value) values (1, '0'), (1, '1');
-insert into operations (first_element_id, second_element_id, result_element_id) VALUES (1, 1, 1), (1, 2, 2), (2, 1, 2), (2, 2, 1);
+INSERT INTO groups (name, order_of_group) VALUES ('Z2', 2);
+INSERT INTO elements (group_id, VALUE) VALUES (1, '0'), (1, '1');
+INSERT INTO operations (first_element_id, second_element_id, result_element_id) VALUES (1, 1, 1), (1, 2, 2), (2, 1, 2), (2, 2, 1);
